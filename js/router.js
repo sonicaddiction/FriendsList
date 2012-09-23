2b63dd54a49ff1ef08e71351f27e839bf219218f
Kristofer Fagerström <sonicaddiction@gmail.com>
2012-09-23 21:51:33 +0200

    Redirect the page to App Authorisation page.

--- js/router.js
+++ js/router.js
@@ -1,4 +1,4 @@
-/*global define: false, require: false */
+/*global define: false, require: false, window: false */
 /*jslint nomen: true */
 
 define(['jquery', 'underscore', 'backbone'], function ($, _, Backbone) {
@@ -14,10 +14,31 @@ define(['jquery', 'underscore', 'backbone'], function ($, _, Backbone) {
             router = new AppRouter(options);
 
         router.on('route:defaultAction', function () {
+            var hash = window.location.hash,
+                facebookAuthenticationUrl = "https://www.facebook.com/dialog/oauth?client_id=501462183199289&redirect_uri=http://localhost/~kristofer/FriendsList&response_type=token";
+
+            if (hash.length > 0 && hash.indexOf("access_token=") === 1) {
+                require(['views/friendsList/friendsListView'], function (FriendsListView) {
+                    var accessToken = hash.substr(1),
+                        friendsListView = new FriendsListView({
+                            accessToken: accessToken
+                        });
+                    $(".pageContent").html(friendsListView.render().el);
+                });
+            } else {
+                window.location.replace(facebookAuthenticationUrl);
+            }
+        });
+
+        router.on('route:friendsList', function () {
             require(['views/friendsList/friendsListView'], function (FriendsListView) {
                 var friendsListView = new FriendsListView();
                 $(".pageContent").html(friendsListView.render().el);
             });
+        });
+
+        router.on('route:authentication', function () {
+            console.log(window);
         });
 
         Backbone.history.start();
